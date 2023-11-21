6c9e511b2be847261fd6dd1a132785bc1c4ddf58
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-21 14:01:14 +0100

    finished controller

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,36 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String DEFAULT_FILE = System.getProperty("user.home")
+        + File.separator
+        + "output.txt";
+    private File file;
 
+    public void saveString(String input) throws IOException {
+        try (PrintStream ps = new PrintStream(file, StandardCharsets.UTF_8)) {
+            ps.print(input);
+        }
+    }
+
+    public String path() {
+        return file.getPath();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
 }
