7716974b77bee9a72fabac7def5d8016a9bcea3c
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-24 20:54:31 +0100

    finished exercise

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -14,6 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 
 /**
@@ -38,8 +39,11 @@ public final class SimpleGUIWithFileChooser {
         final JButton browse = new JButton("Browse");
         top.add(textField, BorderLayout.CENTER);
         top.add(browse, BorderLayout.LINE_END);
-        canvas.add(top, BorderLayout.CENTER);
-
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        canvas.add(top, BorderLayout.NORTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -50,13 +54,26 @@ public final class SimpleGUIWithFileChooser {
                 int returnVal = jFileChooser.showSaveDialog(canvas);
                 if(returnVal == JFileChooser.APPROVE_OPTION) {
                     controller.setFile(jFileChooser.getSelectedFile());
+                    textField.setText(controller.getFile().toString());
                 } else if (returnVal != JFileChooser.CANCEL_OPTION) {
                     JOptionPane.showMessageDialog(browse, e, null, returnVal);
                 }
             }
             
         });
-        
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                System.out.println(textArea.getText());
+                try {
+                    controller.saveString(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+            }
+        });
     }
 
     private void display() {
@@ -71,5 +88,4 @@ public final class SimpleGUIWithFileChooser {
     public static void main(final String... args) {
         new SimpleGUIWithFileChooser().display();
     }
-
 }
