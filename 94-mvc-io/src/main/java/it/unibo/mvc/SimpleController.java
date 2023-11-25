beaf229eb9e80a664f55565f2ed5298e52e53b72
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-25 18:16:29 +0100

    finished SimpleController

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,47 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String currentString = "";
+    private String nextString = "";
+    private final List<String> printed = new ArrayList<>();
+
+    @Override
+    public void print(String input) {
+        System.out.println(input);
+        printed.add(input);
+    }
+
+    public String getNextString() {
+        return nextString;
+    }
+
+    public void setNextString(String nextString) {
+        this.nextString = Objects.requireNonNull(nextString);
+    }
+
+    public void printCurrentString() {
+        if (currentString.equals("")) {
+            throw new IllegalStateException("currentString is unset");
+        }
+        print(currentString);
+    }
+
+    public void printNextString() {
+
+    }
+
+    public List<String> printHistory() {
+
+        return null;
+    }
 
 }
