7e5d74cd101d04c8854ef35cb894b9fb20b1c384
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-25 18:16:11 +0100

    finished controller

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -4,5 +4,9 @@ package it.unibo.mvc;
  *
  */
 public interface Controller {
-
+    /**
+     * Prints on the standard output
+     * @param input String to print on stdout
+     */
+    void print(String input);
 }
